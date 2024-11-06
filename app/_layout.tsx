d253be4ee8c4e569c2c7ab6544d069b6343dc276
meredithstamm <146149589+meredithstamm@users.noreply.github.com>
2024-11-06 15:27:30 -0600

    resolved merge conflict

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,16 @@
+// RootLayout.tsx
+import React from "react";
 import { Stack } from "expo-router";
+import Navbar from "../components/navigation/NavBar";
 
 export default function RootLayout() {
   return (
-    <Stack>
-      <Stack.Screen name="index" options={{ headerShown: false }} />
-    </Stack>
+    <>
+      <Stack>
+        <Stack.Screen name="index" options={{ headerShown: false }} />
+        {/* Add other Stack.Screen components for multiple pages */}
+      </Stack>
+      <Navbar /> {/* Navbar will always be rendered at the bottom of every page */}
+    </>
   );
 }
